d8865c6100738ca28586631ef0e199348567f2d9
katherine011 <guluaq09@gmail.com>
2024-12-08 00:26:30 +0400

    task1 M

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const toggleButton = document.getElementById("toggleButton");
-    const threeTextMenu = document.querySelector(".three-textM");
-  
-    toggleButton.addEventListener("click", () => {
-      threeTextMenu.classList.toggle("show");
-    });
+  const toggleButton = document.getElementById("toggleButton");
+  const threeTextMenu = document.querySelector(".three-textM");
+
+  toggleButton.addEventListener("click", () => {
+    threeTextMenu.classList.toggle("show");
   });
+});
