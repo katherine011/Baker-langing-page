f7d3f65135a393307fce77aa6a25a46a5ef72123
mariam titvinidze <titvinidzemariam3@gmail.com>
2024-12-14 21:38:23 +0400

    bakerM-2

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ document.addEventListener("DOMContentLoaded", () => {
     threeTextMenu.classList.toggle("show");
   });
 });
+
+ 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: '3',
+  spaceBetween: 4,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    375: {
+      slidesPerView: 1, 
+      spaceBetween: 0, 
+    },
+  },
+});
+
+
+
